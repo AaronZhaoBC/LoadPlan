996b16a21fd1ca9f86d2b019aa08b611f1e49f21
AaronZhaoBC <aaronzhaobc@gmail.com>
2026-03-19 17:11:55 +0800

    Add files via upload

--- chatbot_trial.py
+++ chatbot_trial.py
@@ -621,7 +621,7 @@ class SteelLoadingPlanner:
       try:
         response = send_prompt(openai_api_key, system_prompt + user_prompt)
       except:
-        return "Sorry, but you need to insert API key to start conversation. OK"
+        return "Sorry, but you need to insert API key to start conversation. OK" + openai_api_key
 
       return response.text  
 
